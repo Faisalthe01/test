1bcbe3577aa06a3fc9ce118c06290a2d988399d3
Faisal <62862684+Faisalthe01@users.noreply.github.com>
2020-08-29 16:43:37 +1000

    Update GarageDoorAccessory.js

--- GarageDoorAccessory.js
+++ GarageDoorAccessory.js
@@ -42,18 +42,18 @@ class GarageDoorAccessory extends BaseAccessory {
         }
 
         const characteristicTargetDoorState = service.getCharacteristic(Characteristic.TargetDoorState)
-            .updateValue(this._getTargetDoorState(dps['101']))
+            .updateValue(this._getTargetDoorState(dps[this.dpStatus]))
             .on('get', this.getTargetDoorState.bind(this))
             .on('set', this.setTargetDoorState.bind(this));
 
         const characteristicCurrentDoorState = service.getCharacteristic(Characteristic.CurrentDoorState)
-            .updateValue(this._getCurrentDoorState(dps['101']))
+            .updateValue(this._getCurrentDoorState(dps[this.dpStatus]))
             .on('get', this.getCurrentDoorState.bind(this));
 
         this.device.on('change', (changes, state) => {
             console.log('[TuyaAccessory] GarageDoor changed: ' + JSON.stringify(state));
 
-            if (changes.hasOwnProperty('101')) {
+            if (changes.hasOwnProperty(this.dpStatus)) {
                 const newCurrentDoorState = this._getCurrentDoorState(state);
                 if (characteristicCurrentDoorState.value !== newCurrentDoorState) characteristicCurrentDoorState.updateValue(newCurrentDoorState);
             }
@@ -61,7 +61,7 @@ class GarageDoorAccessory extends BaseAccessory {
     }
 
     getTargetDoorState(callback) {
-        this.getState('101', (err, dp) => {
+        this.getState(this.dpStatus, (err, dp) => {
             if (err) return callback(err);
 
             callback(null, this._getTargetDoorState(dp));
@@ -76,11 +76,11 @@ class GarageDoorAccessory extends BaseAccessory {
     }
 
     setTargetDoorState(value, callback) {
-        this.setState(['1'], value === this.targetOpen, callback);
+        this.setState([this.dpAction], value === this.targetOpen, callback);
     }
 
     getCurrentDoorState(callback) {
-        this.getState(['101'], (err, dps) => {
+        this.getState([this.dpStatus], (err, dps) => {
             if (err) return callback(err);
 
             callback(null, this._getCurrentDoorState(dps));
@@ -91,7 +91,7 @@ class GarageDoorAccessory extends BaseAccessory {
         // ToDo: Check other `dps` for opening and closing states
 
 
-        return dps['101'] ? this.currentOpen : this.currentClosed;
+        return dps[this.dpStatus] ? this.currentOpen : this.currentClosed;
 
     }
 }
